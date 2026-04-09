b59145c2c56240db73e9099930f835a6fc16e906
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-08 22:33:45 -0500

    feat: Configuration of ms-leagues service

---
+++ ms-leagues/src/test/java/com/sportspulse/ms_leagues/MsLeaguesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportspulse.ms_leagues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLeaguesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
