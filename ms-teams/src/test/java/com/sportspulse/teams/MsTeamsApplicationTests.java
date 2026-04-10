181e0dead1ea72204974e1bef29148d4a1db72bb
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-09 21:03:09 -0500

    chore: plugin configuration
    - Spotless, Checkstyle, PMD, and JaCoCo are configured in the teams service

--- ms-teams/src/test/java/com/sportspulse/teams/MsTeamsApplicationTests.java
+++ ms-teams/src/test/java/com/sportspulse/teams/MsTeamsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sportspulse.ms_teams;
+package com.sportspulse.teams;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsTeamsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
