854259f3b87a8ee4dcc08b60e0ab110684c1ba0d
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-09 20:46:16 -0500

    chore: plugin configuration
    - Spotless, Checkstyle, PMD, and JaCoCo are configured in the fixtures service

---
+++ ms-fixtures/src/main/java/com/sportspulse/fixtures/MsFixturesApplication.java
@@ -0,0 +1,13 @@
+package com.sportspulse.fixtures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Main application class for Auth Service. */
+@SpringBootApplication
+public class MsFixturesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsFixturesApplication.class, args);
+  }
+}
