510af9589ef5d9050aad9b128578a105784c2f1a
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-10 15:21:39 -0500

    feat(ms-auth): implement RegisterRequest DTO for user registration

---
+++ ms-auth/src/main/java/com/sportspulse/auth/dto/requests/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.sportspulse.auth.dto.requests;
+
+import com.sportspulse.auth.utils.validators.password.ValidPassword;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/** User registration request DTO. */
+@Getter
+@Setter
+@Builder
+public class RegisterRequest {
+
+  @NotBlank(message = "Username is required")
+  private String username;
+
+  @NotBlank(message = "Email is required")
+  private String email;
+
+  @NotBlank(message = "Password is required")
+  @ValidPassword
+  private String password;
+}
