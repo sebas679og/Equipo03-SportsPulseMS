d71909708e566bfd1be2b20f59de08ef0fec9643
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-09 20:52:55 -0500

    chore: plugin configuration
    - Spotless, Checkstyle, PMD, and JaCoCo are configured in the notifications service

---
+++ ms-leagues/src/main/java/com/sportspulse/leagues/MsLeaguesApplication.java
@@ -0,0 +1,13 @@
+package com.sportspulse.leagues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Main application class for Auth Service. */
+@SpringBootApplication
+public class MsLeaguesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsLeaguesApplication.class, args);
+  }
+}
--- ms-leagues/src/test/java/com/sportspulse/leagues/MsLeaguesApplicationTests.java
+++ ms-leagues/src/test/java/com/sportspulse/leagues/MsLeaguesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sportspulse.ms_leagues;
+package com.sportspulse.leagues;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsLeaguesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
