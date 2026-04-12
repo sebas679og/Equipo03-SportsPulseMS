2188c2f76c8e7d4f0bdf5e18c1f7317d2f88b216
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-11 20:33:46 -0500

    feat(ms-auth): creation of request and response DTOs

---
+++ ms-auth/src/main/java/com/sportspulse/auth/dto/responses/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.sportspulse.auth.dto.responses;
+
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Value;
+
+/** LoginResponse Represents the data returned after a successful login. */
+@Value
+@Builder
+public class LoginResponse {
+  String token;
+  String tokenType;
+  long expiresIn;
+  UUID userId;
+}
