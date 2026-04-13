46701b8e3395bf640b0abaf0b45be128dddaa366
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-13 14:33:55 -0500

    chore(ms-gateway): add API paths and internal headers constants

---
+++ ms-gateway/src/main/java/com/sportspulse/gateway/config/constants/InternalHeaders.java
@@ -0,0 +1,9 @@
+package com.sportspulse.gateway.config.constants;
+
+/** InternalHeaders Defines constant values for internal HTTP headers. */
+public final class InternalHeaders {
+
+  private InternalHeaders() {}
+
+  public static final String CLIENT_IP_HEADER = "X-Forwarded-For";
+}
