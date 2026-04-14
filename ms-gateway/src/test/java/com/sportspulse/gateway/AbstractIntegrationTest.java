96071dd63cbd051d19491b26fffeb7a2f758c7cb
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-14 18:12:50 -0500

    redis conatiner

--- ms-gateway/src/test/java/com/sportspulse/gateway/AbstractIntegrationTest.java
+++ ms-gateway/src/test/java/com/sportspulse/gateway/AbstractIntegrationTest.java
@@ -10,39 +10,39 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 /** Abstraction Configuration Redis and wiremock Test. */
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
-@Testcontainers
 public class AbstractIntegrationTest {
 
-  @Container
-  static final RedisContainer REDIS =
-          new RedisContainer(DockerImageName.parse("redis:8.6.2-alpine"))
-                  .waitingFor(Wait.forListeningPort());
-
   protected static final WireMockServer wireMock;
+  private static final RedisContainer REDIS;
 
   static {
+    REDIS =
+        new RedisContainer(DockerImageName.parse("redis:8.6.2-alpine"))
+            .waitingFor(Wait.forListeningPort());
+    REDIS.start();
+
     wireMock = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
     wireMock.start();
   }
 
   @AfterAll
-  static void stopWireMock() {
+  static void stopContainers() {
     wireMock.stop();
+    if (REDIS.isRunning()) {
+      REDIS.stop();
+    }
   }
 
   @DynamicPropertySource
   static void properties(DynamicPropertyRegistry registry) {
     registry.add("spring.data.redis.host", REDIS::getHost);
     registry.add("spring.data.redis.port", REDIS::getFirstMappedPort);
-    registry.add("sportspulse.gateway.services.auth",
-            () -> "http://localhost:" + wireMock.port());
+    registry.add("sportspulse.gateway.services.auth", () -> "http://localhost:" + wireMock.port());
   }
 }
