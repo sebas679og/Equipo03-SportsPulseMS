d9b53e1e80448a08f8e4bf53e5f81854b2c579cf
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-16 16:36:57 -0500

    feat(ms-teams): configuration of properties file and extraction class

---
+++ ms-teams/src/main/java/com/sportspulse/teams/config/properties/TeamsCacheProperties.java
@@ -0,0 +1,21 @@
+package com.sportspulse.teams.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TeamsCacheProperties Configuration properties for the teams caching mechanism. Defines whether
+ * caching is enabled, the time-to-live (TTL) in minutes, and the maximum cache size. Values are
+ * loaded from application configuration using the prefix {@code sportspulse.teams.cache}.
+ */
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "sportspulse.teams.cache")
+public class TeamsCacheProperties {
+  private boolean enabled;
+  private long ttlMinutes;
+  private long maxSize;
+}
