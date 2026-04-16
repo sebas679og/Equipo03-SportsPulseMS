6bd884b95e7dfca7f6fa7a643800188f3dbadcf7
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-15 22:37:32 -0500

    feat(ms-teams): Creation of classes for configuration of header constants

---
+++ ms-teams/src/main/java/com/sportspulse/teams/config/constants/InternalHeaders.java
@@ -0,0 +1,18 @@
+package com.sportspulse.teams.config.constants;
+
+/**
+ * InternalHeaders Defines internal HTTP header constants used for communication between services
+ * and external APIs. Provides centralized keys to ensure consistency and avoid duplication.
+ */
+public final class InternalHeaders {
+
+  /** ApiFootball Contains header constants specific to the Football API integration. */
+  public static final class ApiFootball {
+    public static final String API_FOOTBALL_KEY = "x-apisports-key";
+  }
+
+  /** MsAuth Contains header constants specific to internal authentication between microservices. */
+  public static final class MsAuth {
+    public static final String MS_AUTH_KEY = "X-Internal-API-Key";
+  }
+}
