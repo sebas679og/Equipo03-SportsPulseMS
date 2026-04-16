a2c96bbffa05282a998d68eae2a6a5d369384fe5
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-15 22:01:46 -0500

    feat(ms-teams): Creation of DTO that captures API response, according to documentation

---
+++ ms-teams/src/main/java/com/sportspulse/teams/integration/dto/response/teamid/VenueResponse.java
@@ -0,0 +1,24 @@
+package com.sportspulse.teams.integration.dto.response.teamid;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Value;
+
+/**
+ * VenueResponse
+ * Represents the details of a sports venue.
+ * Contains information such as identity, location, capacity, surface type, and image reference.
+ */
+@Value
+@Getter
+@Builder
+public class VenueResponse {
+    int id;
+    String name;
+    String address;
+    String city;
+    int capacity;
+    String surface;
+    String image;
+}
+
