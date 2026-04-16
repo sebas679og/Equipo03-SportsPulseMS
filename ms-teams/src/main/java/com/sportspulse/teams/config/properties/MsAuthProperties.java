10e357b109e5ed115108e04c7a368bf6d044befc
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-15 22:36:52 -0500

    feat(ms-teams): Creation of classes for configuration and extraction of properties

---
+++ ms-teams/src/main/java/com/sportspulse/teams/config/properties/MsAuthProperties.java
@@ -0,0 +1,20 @@
+package com.sportspulse.teams.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * MsAuthProperties Configuration properties for accessing the internal authentication service.
+ * Provides the base URL and API key, loaded from application configuration using the prefix {@code
+ * sportspulse.teams.services.auth}.
+ */
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "sportspulse.teams.services.auth")
+public class MsAuthProperties {
+  private String baseUrl;
+  private String apiKey;
+}
