b99ec23a3e1140f143f42f0e70431ffe96076e35
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-15 22:35:38 -0500

    feat(ms-teams): Creation of interface and call implementation class for api-football

---
+++ ms-teams/src/main/java/com/sportspulse/teams/integration/client/FootballClient.java
@@ -0,0 +1,18 @@
+package com.sportspulse.teams.integration.client;
+
+import com.sportspulse.teams.integration.dto.response.teamid.ApiFootballTeamResponse;
+
+/**
+ * FootballClient Defines the contract for interacting with the Football API. Provides methods to
+ * retrieve football team information from the external service.
+ */
+public interface FootballClient {
+
+  /**
+   * Retrieves football team details from the Football API by team identifier.
+   *
+   * @param teamId the unique identifier of the team
+   * @return an {@link ApiFootballTeamResponse} containing the team details
+   */
+  ApiFootballTeamResponse getApiFootballTeamById(int teamId);
+}
