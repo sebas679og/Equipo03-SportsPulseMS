b256d5ff594ce108e498c3b8e8fc7d177779efe1
Gabriel <luquegabrielies@gmail.com>
2026-04-22 15:07:00 +0200

    feat: implement ValidateResponse DTO

---
+++ ms-teams/src/main/java/com/sportspulse/teams/dto/internal/ValidateResponse.java
@@ -0,0 +1,12 @@
+package com.sportspulse.teams.dto.internal;
+
+import lombok.Builder;
+
+@Builder
+public record ValidateResponse(
+        Boolean valid,
+        String userId,
+        String username,
+        String role
+) {
+}
