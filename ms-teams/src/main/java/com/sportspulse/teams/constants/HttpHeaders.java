483c99dd222c30f6956ec67d5eeae2db111c3f9c
Gabriel <luquegabrielies@gmail.com>
2026-04-22 14:50:57 +0200

    feat: add HttpHeaders constants

---
+++ ms-teams/src/main/java/com/sportspulse/teams/constants/HttpHeaders.java
@@ -0,0 +1,11 @@
+package com.sportspulse.teams.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class HttpHeaders {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String X_API_SPORTS_KEY = "x-apisports-key";
+    public static final String X_INTERNAL_API_KEY = "X-Internal-API-Key";
+}
