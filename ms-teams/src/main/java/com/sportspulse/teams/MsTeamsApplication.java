4803a7c429ee8c3f66495fdc680be06e2434f465
Gabriel <luquegabrielies@gmail.com>
2026-04-22 15:17:52 +0200

    feat: enable Feign clients and Caching in main application

--- ms-teams/src/main/java/com/sportspulse/teams/MsTeamsApplication.java
+++ ms-teams/src/main/java/com/sportspulse/teams/MsTeamsApplication.java
@@ -2,9 +2,13 @@ package com.sportspulse.teams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /** Main application class for Auth Service. */
 @SpringBootApplication
+@EnableFeignClients
+@EnableCaching
 public class MsTeamsApplication {
 
   public static void main(String[] args) {
