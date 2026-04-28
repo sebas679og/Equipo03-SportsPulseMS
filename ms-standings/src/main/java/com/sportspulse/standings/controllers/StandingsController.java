1154f1445d91171f135409411c255ee99a7c0376
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-27 21:39:13 -0500

    feat(ms-standings): implementation of controller for service

--- ms-standings/src/main/java/com/sportspulse/standings/controllers/StandingsController.java
+++ ms-standings/src/main/java/com/sportspulse/standings/controllers/StandingsController.java
@@ -3,6 +3,7 @@ package com.sportspulse.standings.controllers;
 import com.sportspulse.standings.config.constants.ApiPaths;
 import com.sportspulse.standings.dtos.request.LeagueAndSeasonRequest;
 import com.sportspulse.standings.dtos.responses.StandingsLeagueAndSeasonResponse;
+import com.sportspulse.standings.dtos.responses.TeamStandingLeagueAndSeasonResponse;
 import com.sportspulse.standings.services.StandingsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,13 @@ public class StandingsController {
       @ParameterObject @Valid @ModelAttribute LeagueAndSeasonRequest request) {
     return ResponseEntity.status(HttpStatus.OK)
         .body(standingsService.getStandingsByLeagueAndSeason(request));
+  }
+
+  @GetMapping(ApiPaths.Standings.POSITION_TEAM_IN_THE_STANDINGS)
+  public ResponseEntity<TeamStandingLeagueAndSeasonResponse> getTeamStandingsByLeagueAndSeason(
+      @PathVariable int teamId,
+      @ParameterObject @Valid @ModelAttribute LeagueAndSeasonRequest request) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(standingsService.getTeamStandingsByLeagueAndSeason(request, teamId));
   }
 }
