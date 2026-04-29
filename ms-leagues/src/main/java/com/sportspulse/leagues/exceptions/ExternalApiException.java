f503c2524c28af2a1ca42080dafd7b2fb6e9ffec
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-28 21:54:56 -0500

    feat(ms-leagues): Removal of "recreation" files

--- ms-leagues/src/main/java/com/sportspulse/leagues/exceptions/ExternalApiException.java
+++
@@ -1,9 +0,0 @@
-package com.sportspulse.leagues.exceptions;
-
-/** Exception thrown for API-Football integration failures. */
-public class ExternalApiException extends RuntimeException {
-
-  public ExternalApiException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
