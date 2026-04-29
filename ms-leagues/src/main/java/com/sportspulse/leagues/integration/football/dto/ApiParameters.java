0c1a20482c22e2f93d3c76c2f9582b685864c048
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-04-28 21:58:53 -0500

    feat(ms-leagues): creation of DTOs for capturing API-football response

---
+++ ms-leagues/src/main/java/com/sportspulse/leagues/integration/football/dto/ApiParameters.java
@@ -0,0 +1,9 @@
+package com.sportspulse.leagues.integration.football.dto;
+
+/**
+ * ApiParameters Data transfer object (DTO) representing request parameters provided by the external
+ * football API.
+ *
+ * <p>Contains the unique identifier used to query or filter specific API resources.
+ */
+public record ApiParameters(String id) {}
