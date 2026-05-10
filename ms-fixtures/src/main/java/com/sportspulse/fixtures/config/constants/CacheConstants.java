2ae373c9132fad9fe7f7b5e249603b01f0d191f5
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-09 21:45:24 -0500

    feat(ms-fixtures): Configuration build for api-football cache

---
+++ ms-fixtures/src/main/java/com/sportspulse/fixtures/config/constants/CacheConstants.java
@@ -0,0 +1,10 @@
+package com.sportspulse.fixtures.config.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/** Utility class that defines constant names for the application caches. */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CacheConstants {
+  public static final String FIXTURES_CACHE = "fixture";
+}
