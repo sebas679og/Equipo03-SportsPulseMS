2ccc79aaf2cbee9dc6c0460a6a9e7e8df02fa7d3
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-06-17 21:57:40 -0500

    feat: creation of static class for path routes
    #47

---
+++ ms-fixtures/src/main/java/com/sportspulse/fixtures/config/constants/ApiPaths.java
@@ -0,0 +1,14 @@
+package com.sportspulse.fixtures.config.constants;
+
+public final class ApiPaths {
+
+    private ApiPaths() {}
+
+    /**
+     * AuthService Defines API endpoint paths related to the authentication service. Provides
+     * constants to ensure consistency when building requests.
+     */
+    public static final class AuthService {
+        public static final String VALIDATE_TOKEN = "/api/auth/validate";
+    }
+}
