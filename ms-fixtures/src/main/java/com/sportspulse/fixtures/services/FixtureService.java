d7c8676799b74c009b362c004b07e7661639f2bf
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-06-20 22:58:50 -0500

    feat: creation of fixtures service - initial
    #47

---
+++ ms-fixtures/src/main/java/com/sportspulse/fixtures/services/FixtureService.java
@@ -0,0 +1,4 @@
+package com.sportspulse.fixtures.services;
+
+public interface FixtureService {
+}
---
+++ ms-fixtures/src/main/java/com/sportspulse/fixtures/services/FixtureServiceImpl.java
@@ -0,0 +1,7 @@
+package com.sportspulse.fixtures.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixtureServiceImpl implements FixtureService{
+}
