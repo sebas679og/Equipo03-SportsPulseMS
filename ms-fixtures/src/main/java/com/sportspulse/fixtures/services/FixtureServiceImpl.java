f5a16197ab6734553d34b21144be62d7e4dbb387
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-06-22 11:25:20 -0500

    feat: parsing fix due to use of JsonNode in errors

--- ms-fixtures/src/main/java/com/sportspulse/fixtures/services/FixtureServiceImpl.java
+++ ms-fixtures/src/main/java/com/sportspulse/fixtures/services/FixtureServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sportspulse.fixtures.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.sportspulse.fixtures.dtos.requests.FixturesQueryParamsRequest;
 import com.sportspulse.fixtures.dtos.responses.fixtures.FixturesResponse;
 import com.sportspulse.fixtures.exceptions.CustomBadGatewayException;
@@ -15,7 +16,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Service
@@ -49,30 +49,28 @@ public class FixtureServiceImpl implements FixtureService{
     }
 
     private void handleApiFootballErrors(ApiFixtureResponse api) {
-        if (api.errors() == null || api.errors().isEmpty()) {
+        JsonNode errors = api.errors();
+        if (errors == null || errors.isEmpty() || errors.isArray()) {
             return;
         }
 
-        @SuppressWarnings("unchecked")
-        Map<String, Object> errorDetail = (Map<String, Object>) api.errors().get(0);
-
-        if (errorDetail.containsKey("requests")) {
+        if (errors.has("requests")) {
             log.warn(
                     "Api-Football 429 Rate Limit exceeded. "
                             + "Available requests have been exhausted. Body: {}",
-                    errorDetail);
+                    errors);
             throw new CustomTooManyRequestsException(
                     "The daily request limit to Api-Football has been "
                             + "reached, please try again tomorrow");
-        } else if (errorDetail.containsKey("plan")) {
-            String planMessage = (String) errorDetail.get("plan");
+        } else if (errors.has("plan")) {
+            String planMessage = errors.get("plan").asText();
             log.warn(
                     "Api-Football, the request limit per season has been exceeded. "
                             + "Available requests have been exhausted. Body: {}",
                     planMessage);
             throw new CustomTooManyRequestsException(planMessage);
         } else {
-            log.error("Api-Football returned errors in the response. Body: {}", errorDetail);
+            log.error("Api-Football returned errors in the response. Body: {}", errors);
             throw new CustomBadGatewayException(
                     "An error occurred while processing the request to Api-Football. "
                             + "Please try again later.");
